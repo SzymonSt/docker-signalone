fabb148903d27160c629d69e56f5ae6278e7f74a
SzymonSt <szymonst2808@gmail.com>
2024-01-03 22:06:00 +0100

    feat: server initialization with all subsystems and componenets

--- backend/cmd/config/config.go
+++ backend/cmd/config/config.go
@@ -5,12 +5,26 @@ import (
 )
 
 type Config struct {
+	ServerPort string `mapstructure:"SERVER_PORT"`
+	//Inference Engine API
+	InferenceApiUrl      string `mapstructure:"INFERENCE_API_URL"`
 	HuggingFaceApiKey    string `mapstructure:"HUGGINGFACE_API_KEY"`
 	HuggingFaceBaseModel string `mapstructure:"BASE_MODEL_NAME"`
-	DbHost               string `mapstructure:"DB_HOST"`
-	DbPassword           string `mapstructure:"DB_PASSWORD"`
-	DbUser               string `mapstructure:"DB_USER"`
-	DbName               string `mapstructure:"DB_NAME"`
+
+	//Tokenized Solution for Prediction Database Details
+	SolutionDbHost         string `mapstructure:"SOLUTION_DB_HOST"`
+	SolutionDbName         string `mapstructure:"SOLUTION_DB_NAME"`
+	SolutionCollectionName string `mapstructure:"SOLUTION_COLLECTION_NAME"`
+
+	//Application Database Details
+	ApplicationDbUrl          string `mapstructure:"APPLICATION_DB_URL"`
+	ApplicationDbName         string `mapstructure:"APPLICATION_DB_NAME"`
+	ApplicationCollectionName string `mapstructure:"APPLICATION_COLLECTION_NAME"`
+
+	//Saved Analysis Database Details
+	SavedAnalysisDbUrl          string `mapstructure:"SAVED_ANALYSIS_DB_URL"`
+	SavedAnalysisDbName         string `mapstructure:"SAVED_ANALYSIS_DB_NAME"`
+	SavedAnalysisCollectionName string `mapstructure:"SAVED_ANALYSIS_COLLECTION_NAME"`
 }
 
 func New() (config *Config) {
@@ -21,8 +35,11 @@ func New() (config *Config) {
 
 	err := viper.ReadInConfig()
 	if err != nil {
-		return
+		return nil
 	}
 	err = viper.Unmarshal(&config)
+	if err != nil {
+		return nil
+	}
 	return config
 }
