616dfa2172000a46f7e620e5c121a6857e6478f4
SzymonSt <szymonst2808@gmail.com>
2024-01-20 08:56:29 +0100

    bugfix: main api path duplication

--- backend/pkg/routers/main.router.go
+++ backend/pkg/routers/main.router.go
@@ -17,18 +17,18 @@ func NewMainRouter(mainController *controllers.MainController) *MainRouter {
 }
 
 func (mr *MainRouter) RegisterRoutes(rg *gin.RouterGroup) {
-	authorizationRouterGroup := rg.Group("/api/auth")
+	authorizationRouterGroup := rg.Group("/auth")
 	authorizationRouterGroup.POST("/user/login", func(c *gin.Context) {})
 	authorizationRouterGroup.PUT("/user/register", func(c *gin.Context) {})
 
-	userRouterGroup := rg.Group("/api/user")
+	userRouterGroup := rg.Group("/user")
 	userRouterGroup.GET("/settings", func(c *gin.Context) {})
 	userRouterGroup.POST("/settings", func(c *gin.Context) {})
 	userRouterGroup.GET("/issues", func(c *gin.Context) {})
 	userRouterGroup.GET("/issues/:id", func(c *gin.Context) {})
 	userRouterGroup.POST("/agent/authenticate", func(c *gin.Context) {})
 
-	agentRouterGroup := rg.Group("/api/agent")
+	agentRouterGroup := rg.Group("/agent")
 	agentRouterGroup.PUT("/issues/analysis", mr.mainController.LogAnalysisTask)
 
 }
