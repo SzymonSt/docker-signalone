37a5b210ae7f2faeed9945cf791f272eaa70c0c5
Szymon Stawski <48925621+SzymonSt@users.noreply.github.com>
2024-03-15 00:49:02 +0100

    Update const.go, suggested changes

--- backend/pkg/utils/const.go
+++ backend/pkg/utils/const.go
@@ -1,6 +1,6 @@
 package utils
 
-var WaitlistEntryConfimrationEmail = `
+var WaitlistEntryConfirmationEmail = `
 <!doctype html>
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml"
   xmlns:o="urn:schemas-microsoft-com:office:office">
