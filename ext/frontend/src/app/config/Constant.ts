121a2bffa402c3d28cc0661cbeaaff966b53f26e
paweleksltzn <pawel.antolak@interia.eu>
2024-03-15 09:57:16 +0100

    change: password requirements adjustment

--- ext/frontend/src/app/config/Constant.ts
+++ ext/frontend/src/app/config/Constant.ts
@@ -1,3 +1,5 @@
 export class Constants {
   public static paginationLimit: number = 10;
+  public static PASSWORD_MIN_LENGTH: number = 8;
+  public static PASSWORD_PATTERN: RegExp =  new RegExp(`^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[~!@#$%^&*()_+-]).{${Constants.PASSWORD_MIN_LENGTH},}$`);
 }
