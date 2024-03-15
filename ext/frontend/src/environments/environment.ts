96351351d69ec2f9354ec71634f9447363417c31
SzymonStawski2808 <szymonst2808@MacBook-Pro-Szymon.local>
2024-03-15 17:42:28 +0100

    rev

--- ext/frontend/src/environments/environment.ts
+++ ext/frontend/src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   defaultLanguage: 'en',
   production: false,
-  apiUrl: 'https://api.signaloneai.com/api',
-  authUrl: 'https://api.signaloneai.com/api/auth',
+  apiUrl: 'http://localhost:8080/api',
+  authUrl: 'http://localhost:8080/api/auth',
   agentApiUrl: 'http://localhost:37002/api',
   googleLoginProvider: '359898712853-rcec16l3ivs24rod4hb2kcsr9qvotf3t.apps.googleusercontent.com',
   githubClientId: '6c88a4f9d4868879974e',
